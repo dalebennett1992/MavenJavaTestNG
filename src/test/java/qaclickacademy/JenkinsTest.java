86ab106a22f81576684968d945b5b9f6fa3efe8c
Dale <dale.bennett@wheatleysolutions.co.uk>
2018-10-17 14:59:59 +0100

    new jenkins test class added

---
+++ src/test/java/qaclickacademy/JenkinsTest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest
+{
+    @Test
+    public void TestJenkins()
+    {
+        System.out.println("TestJenkins");
+    }
+
+}
+
